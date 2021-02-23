0c82a45c7ab1124d70d57473e5cece2b42875a6a
Dan Kent <patorn.5@mail.kmutt.ac.th>
2021-02-23 18:25:51 +0700

    springboot initial commit

---
+++ leash-springboot/src/test/java/com/khakai/leash/LeashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khakai.leash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
