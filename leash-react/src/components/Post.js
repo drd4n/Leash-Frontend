5a98cfd2b3e0d51ea47f69616f60ec1af633deb2
Dan Kent <patorn.5@mail.kmutt.ac.th>
2021-03-07 00:45:12 +0700

    [ADD] upload picture form but cannot remove for now

--- leash-react/src/components/Post.js
+++ leash-react/src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
+import ImageUploader from 'react-images-upload'
 
 const PostForm = styled.div`
     align-items: center;
@@ -16,6 +17,7 @@ const Input = styled.input`
 export const Post = () => {
 
     const [postDetail, setPostDetail] = useState('')
+    const [pictures, setPictures] = useState([])
 
     const addToFeed = () => {
         //post ไปที่ url ,object ใน format ของ json (เรียกว่า body)
@@ -26,13 +28,39 @@ export const Post = () => {
         console.log("click post")
         setPostDetail('')
     }
+
+    const onDrop = (picture) => { 
+        setPictures(pictures.concat(picture))
+    }
+
+    const uploadImages = () => {
+         pictures.map(image => {
+            let data = new FormData();
+            data.append("image", image, image.name);
+            console.log(data)
+            return axios.post('http://localhost:3001/post/uploadImage', data);
+        })
+        console.log(pictures)
+        setPictures([])
+    }
+
     return (
         <PostForm>
             <Input placeholder="What do you want to ask?" type="text" value={postDetail} onChange={(event) => {
                 setPostDetail(event.target.value)
             }} />
             <Input placeholder="links" type="text" />
+            <ImageUploader
+                withPreview={true}
+                withIcon={true}
+                name='name'
+                buttonText='Choose images'
+                onChange={onDrop}
+                imgExtension={['.jpg', '.gif', '.png']}
+                maxFileSize={5242880}
+            />
             <button onClick={addToFeed}>add to mongo db via mongoose</button>
+            <button onClick={uploadImages}>Upload Images</button>
         </PostForm>
     )
 }
