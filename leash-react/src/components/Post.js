e6317536bd6fffbd00fdccfa11933284c9f4553b
Dan Kent <patorn.5@mail.kmutt.ac.th>
2021-05-12 03:29:48 +0700

    [UPDATE] post js able to receive base64 image but not able to show yet

--- leash-react/src/components/Post.js
+++ leash-react/src/components/Post.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
 import ImageUploader from 'react-images-upload'
@@ -18,17 +18,14 @@ export const Post = () => {
 
     const [postDetail, setPostDetail] = useState('')
     const [uploadedpics, setUploadedpics] = useState([])
+    const [isDirty, setisDirty] = useState(false)
 
-    async function uploadText(arrayOfLinks) {
+    async function uploadText() {
         console.log("detail: " + postDetail)
-        console.log(arrayOfLinks[0])
-        console.log({
-            post_text: postDetail,
-            picture_link: arrayOfLinks
-        })
+        console.log(uploadedpics)
         const data = {
             post_text: postDetail,
-            picture_link: arrayOfLinks
+            picture_link: uploadedpics
         }
         console.log("data = ")
         console.log(JSON.stringify(data))
@@ -47,26 +44,32 @@ export const Post = () => {
             formData.append("image", selectedImage, selectedImage.name)
             console.log(formData)
             axios.post('http://localhost:3001/post/uploadImage', formData)
-                //.then(res => console.log(res.data.post_link))
-                //.then(res => setPostLink(res.data.post_link))
                 .then(res => {
                     uploadedpics.push(res.data.picture_link)
-                    console.log(JSON.stringify(uploadedpics))
+                    console.log(JSON.stringify(uploadedpics)) 
+                    setisDirty(true) 
                 })
-        //})
         return temp;
 }
 
-// const onDrop = (picture) => {
-//     setPictures(pictures.concat(picture))
-// }
+useEffect(() => {
+    if(isDirty){
+        setUploadedpics(uploadedpics)
+        axios.get(`http://localhost:3001/post/showSelectedImage/${uploadedpics[0]}`)
+        .then(res => console.log(res))
+        setisDirty(false)
+    }
+},[isDirty, uploadedpics])
 
 return (
     <PostForm>
-        <Input placeholder="What do you want to ask?" type="text" value={postDetail} onChange={(event) => {
+        <Input 
+            placeholder="What do you want to ask?" 
+            type="text" 
+            value={postDetail} 
+            onChange={(event) => {
             setPostDetail(event.target.value)
         }} />
-        <Input placeholder="links" type="text" />
         {/* <ImageUploader
                 withPreview={true}
                 withIcon={true}
@@ -84,16 +87,18 @@ return (
             }}
             style={{ display: 'none' }}
         />
-        {uploadedpics.map(shower =>{
-            return <img key={shower.index} src={shower} alt={shower.name} />
-        })}
+        {
+            uploadedpics.map(shower =>{
+                return <img key={shower.index} src={shower} alt={shower.name} />
+                    }
+                )
+        }
         <button onClick={() => { document.getElementById('selectedFile').click(); }}>Pick File</button>
         <button onClick={uploadText}>add to mongo db via mongoose</button>
         {/* <button onClick={uploadImages}>Upload Images</button> */}
     </PostForm>
-)
+    )
 }
-
 export default Post
 
 
