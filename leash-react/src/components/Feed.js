41302f2b755d044f76829755b92a665d7e1d44a9
Dan Kent <patorn.5@mail.kmutt.ac.th>
2021-05-19 01:51:34 +0700

    [UPDATE] add feed component

--- leash-react/src/components/Feed.js
+++ leash-react/src/components/Feed.js
@@ -1,8 +1,30 @@
-import React from 'react'
-import Post from './PostForm'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export const Feed = () => {
+    const [posts, setPosts] = useState([])
 
+useEffect( () => {
+        // const feed = await axios('http://localhost:3001/post', );
+         axios('http://localhost:3001/post')
+        // setPosts(feed.data)
+        .then(res => {
+            setPosts(res.data)
+            console.log(res.data)
+        }).catch((error) => {
+            console.log(error)
+        })
+},[])
+    
+    return ( 
+        <div>
+            {
+                posts.map((post,i) => {
+                    return <p key={i}>{post.post_text}</p>
+                })
+            }
+        </div>
+    )
 }
 
 export default Feed
