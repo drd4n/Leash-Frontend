4ad75868d600eb581c59fba24f36bc8603990397
Dan Kent <patorn.5@mail.kmutt.ac.th>
2021-05-18 17:55:39 +0700

    [UPDATE] recreate feed functional component

--- leash-react/src/components/Feed.js
+++ leash-react/src/components/Feed.js
@@ -1,7 +1,8 @@
-import React from 'react'
-import Post from './Post'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
+export const Feed = () => {
 
-export const addToFeed = (postDetail) => {
-    alert(postDetail)
 }
+
+export default Feed
