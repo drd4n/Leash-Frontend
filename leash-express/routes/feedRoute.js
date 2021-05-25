88ee0d107435de2068c37868b9c196093d95a22e
Dan Kent <patorn.5@mail.kmutt.ac.th>
2021-05-25 21:59:20 +0700

    [UPDATE] remove unused library

--- leash-express/routes/feedRoute.js
+++ leash-express/routes/feedRoute.js
@@ -2,10 +2,6 @@ const express = require('express')
 const mongoose = require('mongoose')
 const router = express.Router();
 const cors = require('cors');
-const { v4: uuidv4 } = require('uuid');
-const aws = require('aws-sdk');
-const config = require('../config/s3config')
-const createError = require('http-errors')
 const app = express()
 
 app.use(express.json());
