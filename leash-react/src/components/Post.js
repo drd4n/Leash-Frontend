d65b27efa7fdbccdd30baa25760d8b23d6bac889
poonynh_ <poon.roon3@mail.kmutt.ac.th>
2021-05-29 01:59:22 +0700

    [UPDATE] Post flex layout

--- leash-react/src/components/Post.js
+++ leash-react/src/components/Post.js
@@ -3,15 +3,16 @@ import styled, { css } from 'styled-components'
 import axios from 'axios'
 
 const Box = styled.div` 
-    width: 300px;
+    width: 600px;
     height: 250px;
     margin: 20px;
+    padding:10px;
     box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
   `
 const PostImg = styled.img`
-    width: 300px;
+    width: 150px;
     height: 150px;
-    padding: 0px;
+    padding: 5px;
     margin: 0px;
   `
 const PostText = styled.h1`
@@ -21,6 +22,10 @@ const PostText = styled.h1`
 const Time = styled.p`
     font-size: 10px;
     margin:5px;
+`
+
+const PictureLayout = styled.div`
+  display: flex;
 `
 
 export const Post = (props) => {
@@ -38,13 +43,15 @@ export const Post = (props) => {
 console.log(props.post._id) //เอาอันนี้ส่งมาตอนเข้าหน้าโพสแยกกับตอนคอมเม้นด้วย
     return ( 
         <Box>
-        {
+            <PictureLayout>
+            {
                 Imgs.map((img,i) => {
                     return(
                         <PostImg key={i} className="img" src={img}></PostImg>
                     )
                 })
             }
+            </PictureLayout>
             <PostText>{props.post.post_text}</PostText>
             <Time>date XX/XX/XX time XX:XX</Time>
             
