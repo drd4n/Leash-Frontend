c3f6f49e126ffc4dedec611d43ea44bd8f0eab88
drd4n <patorn.5@mail.kmutt.ac.th>
2021-09-22 02:37:31 +0700

    ADD logout button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,11 +18,29 @@ export const Navbar = ({setWillFetch}) => {
         }
         
     }
+
+    function logout() {
+        // if (!TokenValidate()) {
+        //     return alert("session out of date")
+        // }
+         axios.post("http://localhost:3001/auth/logout", {
+                token: localStorage.getItem('token')
+            })
+            .then((res) => {
+                if(res.status === 200){
+                    localStorage.clear()
+                    return document.getElementById("logout").click()
+                }
+            })
+    }
+
     return (
         <>
             <button onClick={() => setWillFetch(true)}>Leash</button>
             <button onClick={() => whoAmI()}>profile</button>
+            <button onClick={() => logout()}>Logout</button>
             <Link id="profile" to={{pathname:"/profile", profile:profile}}></Link>
+            <Link id="logout" to="/login"></Link>
         </>
     )
 }
