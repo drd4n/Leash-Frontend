fade407e703fb989ae1f0bd07e73fce157db55eb
chickenrap <khunanon_43000@hotmail.com>
2021-09-27 06:26:30 +0700

    Styled Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,6 +3,150 @@ import validator from 'validator'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import axios from 'axios'
+import styled, { keyframes, createGlobalStyle } from "styled-components";
+
+
+const jump = keyframes`
+  from{
+    transform: translateY(0)
+  }
+  to{
+    transform: translateY(-3px)
+  }
+`;
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    background: #18191A;
+    ;
+  }
+
+  body, html, #root {
+    font-family: 'Roboto', sans-serif;
+    height: 100%;
+    width: 100%;
+    ;
+  }
+`;
+
+const Container = styled.section`
+    display: flex;
+    justify-content: space-around;
+    align-item: center;
+    align-content: center;
+    flex-wrap: wrap;
+    flex-shrink: 0;
+    height: 100%;
+    width: 100%;
+  
+`;
+
+const WrapperContainer2 = styled.section`
+    display: flex;
+    flex-direction: column;
+    align-item: center;
+    align-content: center;
+    justify-content: center;
+    flex-shrink: 0;
+    flex-grow: 1;
+    align-self: center;
+`;
+
+const Wrapper = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    flex-shrink: 0;
+    align-self: center;
+`;
+
+const Form = styled.form`
+    width: 100%;
+    max-width: 414px;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+`;
+
+const Label = styled.label`
+    color: #FFFFFF;
+    margin-bottom: -0.8rem;
+`;
+
+const FontTitle1 = styled.label`
+    font-style: medium;
+    font-weight: 500;
+    font-size: 42px;
+    line-height: 75px;
+    text-align: center;
+    letter-spacing: 0.05em;
+    color: #FFFFFF;
+`;
+
+const Input = styled.input`
+    max-width: 100%;
+    min-width: 100%;
+    padding: 8px 10px;
+    background: #000000;
+    color: #FFFFFF;
+    margin-bottom: 0rem;
+    border-radius: 10px;
+    outline: 0;
+    border: 1px solid rgba(0, 0, 0, 0);
+    font-size: 14px;
+    transition: all 0.3s ease-out;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.1), 0 1px 1px rgba(0, 0, 0, 0.1);
+    :focus,
+    :hover {
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.15), 0 0px 2.5px rgba(255, 255, 255, 255);
+        
+    }
+`;
+
+const BoxButton = styled.section`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    margin: 0rem;
+    padding: 0;
+`;
+
+const Button = styled.button`
+    max-width: 100%;
+    min-width: 70%;
+    padding: 11px 35px;
+    color: ${(props) => props.textColor};
+    font-weight: 600;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    background: ${(props) => props.backgroundColor};
+    border: none;
+    border-radius: 44px;
+    outline: 0;
+    cursor: pointer;
+    margin-top: 0.5rem;
+    margin-Bottom: 1.5rem;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1), 0 1px 2px rgba(0, 0, 0, 0.1);
+    transition: all 0.3s ease-out;
+    :hover {
+        background: #A1D3CD;
+        color: #FFFFFF;
+        animation: ${jump} 0.2s ease-out forwards;
+    }
+`;
+
+
+const Border = styled.h2`
+    border-style: solid;
+    border-width: 1px;
+    border-color: #5D8888;
+`;
 
 export const Register = () => {
 
@@ -66,74 +210,94 @@ export const Register = () => {
 
     return (
         <>
-            <label htmlFor="firstname">First name</label><br />
-            <input
-                type="text"
-                id="firstname"
-                name="firstname"
-                onChange={(event) => { setForm({ ...form, firstname: event.target.value.trim() }) }} /><br />
+            
+            
+            <GlobalStyle />
 
-            <label htmlFor="lastname">Last name</label><br />
-            <input
-                type="text"
-                id="lastname"
-                name="lastname"
-                onChange={(event) => { setForm({ ...form, lastname: event.target.value.trim() }) }} /><br />
+            <Container>
 
-            <label htmlFor="email">E-mail</label><br />
-            <input
-                type="email"
-                id="email"
-                name="email"
-                onChange={(event) => { setForm({ ...form, email: event.target.value.trim() }) }} /><br />
+            <WrapperContainer2>
+            
+            <Wrapper>
+                <Form>
+                <FontTitle1>Login</FontTitle1>
+                
+                    <Label htmlFor="firstname">First name</Label><br />
+                    <Input
+                        type="text"
+                        id="firstname"
+                        name="firstname"
+                        onChange={(event) => { setForm({ ...form, firstname: event.target.value.trim() }) }} /><br />
 
-            <label htmlFor="dob">Birth date</label><br />
-            <DatePicker
-                id="dob"
-                name="dob"
-                selected={form.dob}
-                onChange={(date) => { setForm({...form, dob: date})}}
-                dateFormat='dd-MM-yyyy'
-                maxDate={new Date()}
-                showMonthDropdown
-                showYearDropdown
-                dropdownMode="select" /><br />
+                    <Label htmlFor="lastname">Last name</Label><br />
+                    <Input
+                        type="text"
+                        id="lastname"
+                        name="lastname"
+                        onChange={(event) => { setForm({ ...form, lastname: event.target.value.trim() }) }} /><br />
 
-            <label htmlFor="username">Username</label><br />
-            <input
-                type="text"
-                id="username"
-                name="username"
-                onChange={(event) => { setForm({ ...form, username: event.target.value.trim() }) }} /><br />
+                    <Label htmlFor="email">E-mail</Label><br />
+                    <Input
+                        type="email"
+                        id="email"
+                        name="email"
+                        onChange={(event) => { setForm({ ...form, email: event.target.value.trim() }) }} /><br />
 
-            <label htmlFor="password">Password</label><br />
-            <input
-                type="password"
-                id="password"
-                name="username"
-                onChange={(event) => { setForm({ ...form, password: event.target.value.trim() }) }} /><br />
+                    <Label htmlFor="dob">Birth date</Label><br />
+                    <DatePicker
+                        id="dob"
+                        name="dob"
+                        selected={form.dob}
+                        onChange={(date) => { setForm({...form, dob: date})}}
+                        dateFormat='dd-MM-yyyy'
+                        maxDate={new Date()}
+                        showMonthDropdown
+                        showYearDropdown
+                        dropdownMode="select" /><br />
 
-            <label htmlFor="crPassword">Confirm Password</label><br />
-            <span>
-                <input
-                    type="password"
-                    id="crPassword"
-                    name="crPassword"
-                    onChange={(event) => { setForm({ ...form, crPassword: event.target.value.trim() }) }} />
+                    <Label htmlFor="username">Username</Label><br />
+                    <Input
+                        type="text"
+                        id="username"
+                        name="username"
+                        onChange={(event) => { setForm({ ...form, username: event.target.value.trim() }) }} /><br />
 
-                <label>
-                    {
-                        // password !== crPassword ? "password not match" : ""
-                        checkPassword()
-                    }
-                </label>
-            </span>
-            <div>
-                {
-                    error
-                }
-            </div>
-            <button onClick={() => validate()}>Register</button>
+                    <Label htmlFor="password">Password</Label><br />
+                    <Input
+                        type="password"
+                        id="password"
+                        name="username"
+                        onChange={(event) => { setForm({ ...form, password: event.target.value.trim() }) }} /><br />
+
+                    <Label htmlFor="crPassword">Confirm Password</Label><br />
+                    <span>
+                        <Input
+                            type="password"
+                            id="crPassword"
+                            name="crPassword"
+                            onChange={(event) => { setForm({ ...form, crPassword: event.target.value.trim() }) }} />
+
+                        <Label>
+                            {
+                                // password !== crPassword ? "password not match" : ""
+                                checkPassword()
+                            }
+                        </Label>
+                    </span>
+                    <div>
+                        {
+                            error
+                        }
+                    </div>
+                    
+                    <BoxButton>
+                    <Button textColor="#5D8888" backgroundColor="#FFFFFF" onClick={() => validate()}>Confirm</Button>
+                    </BoxButton>
+                </Form>
+            </Wrapper>
+            
+            </WrapperContainer2>
+            </Container>
         </>
     )
 }
