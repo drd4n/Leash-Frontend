f30144601935e64d23255722ce2ac293b8f360d7
drd4n <patorn.5@mail.kmutt.ac.th>
2021-09-27 05:38:52 +0700

    add profile picture to effect

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,11 +4,21 @@ import { useLocation } from 'react-router'
 export const Profile = () => {
     const location = useLocation()
     const profile = location.profile
+    const [src, setSrc] = useState("")
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/auth/showProfileImage/${profile.profile_picture}`, {
+            headers: { 'x-access-token': localStorage.getItem('token') }
+        })
+        .then((res) => {
+            setSrc(res.data.profile_picture)
+        })
+    }, [])
 
     return (
         <div>
-         {JSON.stringify(profile)}
-         PROFILE
+            {JSON.stringify(profile)}
+            PROFILE
         </div>
     )
 }
